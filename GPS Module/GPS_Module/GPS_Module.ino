1a69ce115a35d7bb48bc4798ea6d688d47649b4e
Damian <damianvv@live.nl>
2019-05-28 18:16:04 +0200

    GPS

--- GPS Module/GPS_Module/GPS_Module.ino
+++ GPS Module/GPS_Module/GPS_Module.ino
@@ -1,4 +1,5 @@
  #include "Adafruit_GPS.h" 
+ #include "SimpleCAN.h"
 // this sketch will allow you to bypass the Atmega chip
 // and connect the GPS directly to the USB/Serial
 // chip converter.
@@ -9,25 +10,28 @@
 // Connect GPS TX (data out from GPS) to Digital 1
 #define GPSSerial Serial
 Adafruit_GPS GPS(&GPSSerial);
+CANMSG CanMsg; 
+SimpleCAN* can;
 uint32_t timer = millis();
 void setup() 
 {
+  can = new SimpleCAN();
+  can->Setup();
   GPS.begin(9600);
-  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
   GPS.sendCommand(PGCMD_ANTENNA);  
 }
 void loop() 
 {
+  //Reading GPS Data
   if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
-    Serial.println(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
-    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
-      return; // we can fail to parse a sentence in which case we should just wait for another
+    //Serial.println(GPS.lastNMEA()); 
+    if (!GPS.parse(GPS.lastNMEA()))
+      return; 
   }
+
+  //Parsing GPS Data
   if (millis() - timer > 2000) {
   timer = millis(); // reset the timer
     Serial.print("Location: ");
@@ -35,4 +39,24 @@ void loop()
     Serial.print(", ");
     Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);  
   }
+
+  //Received Message Handling
+  if (can->HandleReceivedMessage(&CanMsg)) 
+  {
+    //Message received
+    Serial.print("Local time: ");
+    Serial.print(millis());
+    Serial.println(",");
+    Serial.print("GPS Time: ");
+    Serial.print(GPS.hour, DEC); Serial.print(':');
+    Serial.print(GPS.minute, DEC); Serial.print(':');
+    Serial.print(GPS.seconds, DEC); Serial.print('.');
+    Serial.print("\nCan Message: "); 
+  }
+}
+
+//PLACEHOLDER
+void Emergency() 
+{
+    
 }
---
+++ Radio Module/Radio_Module/Radio_Module.ino
@@ -0,0 +1,11 @@
+#include "Si4703_Breakout.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
