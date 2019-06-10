177eefa624f393b7c36f443ebea1ce0afa36470a
GvanLeeuwen <gerritv65@gmail.com>
2019-06-10 20:00:36 +0100

    mogelijkheid van lange arrays/strings te versturen toegevoegd

--- Core/SimpleCAN/SimpleCAN.cpp
+++ Core/SimpleCAN/SimpleCAN.cpp
@@ -1,24 +1,23 @@
+// to do
+/*
+List ID is nu UniqueSenderID, dit kan beter het address waaop het bericht binnenkomt op de CANbus zijn. En daaronder kunnen dan beter verschillende lijsten met datalijsten komen. Dus eigenlijk een drie dimensionale linked list
+Dus dan is het:
+Lists met CANbus addressen
+   Lists met MessageID
+      Lists met Data
+
+ */
+
 #include "SimpleCAN.h"
 #include "SPI.h"
 
-
 SimpleCAN::SimpleCAN(int canTimeout)
+    : canTimeout(canTimeout)
 {
-   Serial.println("Got here");
+   lists = NULL;
+   extendedMessageTimeout = 1000;
+   Serial.println("SimpleCAN instance created.");
 }
-
-/*bool SimpleCAN::HandleTextMsg(CANMSG msg) //plakt nog niks aan elkaar
-{
-    Serial.print("Received msg: ID: ");
-    Serial.print(msg.adrsValue);
-    Serial.print(" MSG: ");
-    //printByteArray(&msg.data[0], msg.dataLength);
-    Serial.print("\n");
-    return true;
-} */
-
-
-
 
 void SimpleCAN::printByteArray(byte *arrayPtr, int size)
 {
@@ -30,7 +29,7 @@ void SimpleCAN::printByteArray(byte *arrayPtr, int size)
 
 void SimpleCAN::Setup()
 {
-      SPI.setClockDivider(SPI_CLOCK_DIV8);
+   SPI.setClockDivider(SPI_CLOCK_DIV8);
 
   if (can.initCAN(CAN_BAUD_100K) == 0)
   {
@@ -45,7 +44,7 @@ void SimpleCAN::Setup()
     while (1);
   }
 
-  Serial.print("Done setting up\n");
+  Serial.print("SimpleCAN Setup done\n");
 }
 
 bool SimpleCAN::GetReceivedMessage(CANMSG *msg)
@@ -55,65 +54,160 @@ bool SimpleCAN::GetReceivedMessage(CANMSG *msg)
 
 bool SimpleCAN::SendCharMsg(char buffer[], int size,  unsigned long adrsValue)
 {
-    CANMSG messageToSend;
-  messageToSend.adrsValue = adrsValue;
-  messageToSend.isExtendedAdrs = false;
-  messageToSend.rtr = false;
-  messageToSend.dataLength = size;
-  for(int i = 0; i < size; i++)
-  {
-    messageToSend.data[i] = buffer[i];
-    Serial.print(buffer[i]);
-  }
-  Serial.println();
-  return can.transmitCANMessage(messageToSend, 1000);
-
+   return SendByteMessage((byte*)buffer, size, adrsValue);
 }
 
-bool SimpleCAN::SendString(char buffer[], int size, uint8_t uniqueID, unsigned long adrsValue)
+bool SimpleCAN::SendByteMessage(byte buffer[], int size, unsigned long adrsValue) //kapoet???
 {
-  int index = 0;
-  Serial.print("size = ");
-  Serial.println(size);
-  Serial.print("adr = ");
-  Serial.println(adrsValue);
-  while(size > 0)
-  {
-     if(size > 8){
-      if(SendCharMsg(buffer + index, 8, adrsValue))
-      {
-         Serial.print("message sent:");
-      }
-      else
-      {
-         Serial.println("message send failed");
-      }
-    
-     }
-     else
-     {
-       if(SendCharMsg(buffer + index, size, adrsValue))
-    {
-       Serial.println("message sent:");
-    }
-       else
-  {
-     Serial.println("message send failed");
-  }
-     }
-     
- 
-    
-    index += 8;
-    size -= 8;
-  }
+   // Serial.print("byte size: ");
+   // Serial.println(size);
+
+   // Serial.print("byte Sending data: ");
+   // for (size_t i = 0; i < size; i++)
+   // {
+   //    Serial.print(buffer[i], HEX);
+   //    Serial.print(' ');
+   // }
+   // Serial.println();
+
+   CANMSG messageToSend;
+   messageToSend.adrsValue = adrsValue;
+   messageToSend.isExtendedAdrs = false;
+   messageToSend.rtr = false;
+   messageToSend.dataLength = size;
+   for (int i = 0; i < size; i++)
+   {
+      messageToSend.data[i] = buffer[i];
+      // Serial.print(buffer[i]);
+   }
+   // Serial.println();
+   bool result = SendCANMSG(messageToSend);
+
+   if(!result)
+   {
+      Serial.println("CAN doet raar");
+   }
+
+   return result;
 }
 
+bool SimpleCAN::SendPartOfLongByteMessage(byte buffer[], int size, byte senderID, byte messageNumber, unsigned long adrsValue)
+{
+   if (size > 6 || size < 1)
+   {
+      Serial.println("Invalid size in SendPartOfLongByteMessage");
+      return false;
+   }
+   if (buffer == NULL)
+   {
+      Serial.println("Invalid buffer in SendPartOfLongByteMessage");
+      return false;
+   }
+   if(senderID < 0 || senderID > 255)
+   {
+      Serial.println("Invalid senderID in SendPartOfLongByteMessage");
+      return false;
+   }
+   
+   // Serial.print("size: ");
+   // Serial.println(size);
 
+   // Serial.print("senderID: ");
+   // Serial.println(senderID);
+
+   // Serial.print("MessageNumber: ");
+   // Serial.println(messageNumber);
+
+   // Serial.print("Sending data: ");
+   // for (size_t i = 0; i < (size_t)size; i++)
+   // {
+   //    Serial.print(buffer[i], HEX);
+   //    Serial.print(' ');
+   // }
+   // Serial.println();
+
+   byte arr[size + 2];
+   arr[0] = senderID;
+   arr[1] = messageNumber;
+   for (size_t i = 0; i < (size_t)size; i++)
+   {
+      arr[i+2] = buffer[i];
+   }
+
+   // Serial.print("Arr data: ");
+   // for (size_t i = 0; i < size+2; i++)
+   // {
+   //    Serial.print(arr[i], HEX);
+   //    Serial.print(' ');
+   // }
+   // Serial.println();
+
+   if (!SendByteMessage(arr, size+2, adrsValue))
+   {
+      return false;
+   }
+   return true;
+}
+
+bool SimpleCAN::SendLongByteMessage(byte buffer[], int size, unsigned long adrsValue, int senderID)
+{
+   if (size > (255 * 6))
+   {
+      Serial.println("size too big");
+      return false;
+   }
+   if (size < 1)
+   {
+      Serial.println("Can't send empty messages");
+      return false;
+   }
+   if (buffer == NULL)
+   {
+      return false;
+   }
+   
+   bool spareBytes = size % 6 > 0;
+
+   // Send first message with amount of total messages
+   byte NumberOfMessages = size / 6 + spareBytes;
+   SendPartOfLongByteMessage(&NumberOfMessages, 1, senderID, 0, adrsValue);
+
+   // Serial.print("NumberOfMessages: ");
+   // Serial.println(NumberOfMessages);
+
+   byte messageNumber = 0;
+   for (; messageNumber < (size / 6); messageNumber++)
+   {
+      if(!SendPartOfLongByteMessage(buffer + (messageNumber * 6), 6, senderID, messageNumber+1, adrsValue))
+      {
+         Serial.println("Could not send part of long message");
+         return false;
+      }
+   }
+
+   if (spareBytes)
+   {
+      int bytesLeft = size - (messageNumber * 6);
+
+      if (!SendPartOfLongByteMessage(buffer + (messageNumber * 6), bytesLeft, senderID, messageNumber + 1, adrsValue))
+      {
+         Serial.println("Could not send spare bytes of long message");
+         return false;
+      }
+   }
+   return true;
+}
+
+bool SimpleCAN::SendString(char buffer[], int size, unsigned long adrsValue, int senderID)
+{
+   return SendLongByteMessage((byte*)buffer, size, adrsValue, senderID);
+}
 
 bool SimpleCAN::SendCANMSG(CANMSG msg)
 {
-   can.transmitCANMessage(msg, 1000);
+   // Serial.print("Sending CANMSG data: ");
+   // printByteArrayHex(msg.data, msg.dataLength);
+   return can.transmitCANMessage(msg, canTimeout);
 }
 
 bool SimpleCAN::SendInt(int val, unsigned long address)
@@ -141,18 +235,18 @@ CANMSG SimpleCAN::Uint32ToCANMSG(uint32_t val)
    return msg;
 }
 
- CANMSG SimpleCAN::IntToCANMSG(int val)
- {
-     CANMSG msg;
+CANMSG SimpleCAN::IntToCANMSG(int val)
+{
+   CANMSG msg;
    msg.data[0] = val >> 8;
    msg.data[1] = val;
    return msg;
- }
+}
 
- int SimpleCAN::CANMSGToInt(CANMSG msg)
- {
-    return (msg.data[0] << 8) + (msg.data[1]);
- }
+int SimpleCAN::CANMSGToInt(CANMSG msg)
+{
+   return (msg.data[0] << 8) + (msg.data[1]);
+}
 
 CANMSG SimpleCAN::Uint32ToCANMSGWithAddress(uint32_t val, unsigned long address)
 {
@@ -168,8 +262,387 @@ bool SimpleCAN::CANMSGToBool(CANMSG msg)
 }
 
  CANMSG SimpleCAN::BoolToCANMSG(bool val)
- {
-    CANMSG msg;
-    msg.data[0] = val;
-    return msg;
- }
+{
+   CANMSG msg;
+   msg.data[0] = val;
+   return msg;
+}
+
+bool SimpleCAN::CANMSGToByteArray(CANMSG msg, byte** arr, int *size)
+{
+   int listID = msg.data[0];
+   MSGDATALIST* list = FindList(listID);
+   bool listExists = list != NULL;
+   
+   if (msg.data[1] == 0) // Makes a new list when the length message is received
+   {
+      // Serial.print("Expected Listsize: ");
+      // Serial.println(msg.data[2]);
+
+      if (listExists) // edit list size if it already exists
+      {
+         list->expectedListSize = msg.data[2];
+      }
+      if(CreateNewList(listID, msg.data[2]) != 0)
+      {
+         Serial.println("Failed to create a list to store extendedMessage data with length message.");
+      }
+      else
+      {
+         // Serial.println("List has been made.");         
+      }
+      
+      return false;
+   }
+   else if (!listExists) // also makes a new list if length message happens to not be the first to be received
+   {
+      if (CreateNewList(listID, 255) != 0)
+      {
+         Serial.println("Failed to create a list to store extendedMessage data for unaligned message.");
+      }
+   }
+
+   MSGDATA *newData = new MSGDATA;
+   newData->data = new byte[msg.dataLength];
+   
+   newData->dataID = msg.data[1];
+   newData->size = msg.dataLength-2;
+   newData->next = NULL;
+   for (size_t i = 0; i < (size_t)msg.dataLength; i++)
+   {
+      newData->data[i] = msg.data[i+2];
+   }
+
+   int result = AddMsgData(listID, newData, arr, size);
+   switch (result)
+   {
+   case 1:
+      // Serial.println("Full message has been received.");
+      return true;
+      break;
+   case 0:
+      // Serial.print("Data has been added to list: ");
+      // Serial.println(listID);
+      // PrintList(listID);
+      break;
+   case -1:
+      Serial.println("This list does not exist yet.");
+      break;
+   case -2:
+      Serial.println("NewData is NULL or Invalid");
+      break;
+   case -3:
+      Serial.println("Data with those IDs already exists");
+      break;
+
+   default:
+      break;
+   }
+   return false;
+}
+
+void SimpleCAN::printByteArrayHex(byte *data, int length) // prints 8-bit data in hex
+{
+   for (size_t i = 0; i < (size_t)length; i++)
+   {
+      Serial.print(data[i], HEX);
+      Serial.print(' ');
+   }
+}
+
+int SimpleCAN::PrintList(int listID)
+{
+   MSGDATALIST* list = FindList(listID);
+   if(list == NULL)
+   {
+      return -1;
+   }
+
+   Serial.println("=== PRINTING LIST ===");
+
+   Serial.print("ListID: ");
+   Serial.println(list->listID);
+
+   Serial.print("ExpectedListSize: ");
+   Serial.println(list->expectedListSize);
+
+   Serial.print("ListSize: ");
+   Serial.println(list->listSize);
+
+   Serial.print("totalDataSize: ");
+   Serial.println(list->totalData);
+
+   Serial.print("LastWritingTime (ms): ");
+   Serial.println(list->lastWritingTime);
+
+   MSGDATA* currentData = list->dataList;
+   while (currentData != NULL)
+   {
+      Serial.println();
+      Serial.print("Data ID: ");
+      Serial.println(currentData->dataID);
+
+      Serial.print("Data size: ");
+      Serial.println(currentData->size);
+
+      Serial.print("Data: ");
+      printByteArrayHex(currentData->data, currentData->size);
+      Serial.println();
+
+      currentData = currentData->next;
+   }
+   Serial.println("=== === === === === ===");
+   return 0;
+}
+
+int SimpleCAN::CreateNewList(int listID, int expectedListSize)
+{
+   MSGDATALIST *newList = new MSGDATALIST;
+   newList->listID = listID;
+   newList->listSize = 0;
+   newList->totalData = 0;
+   newList->dataList = NULL;
+   newList->expectedListSize = expectedListSize;
+   newList->lastWritingTime = millis();
+
+   return InsertNewList(newList);
+}
+
+int SimpleCAN::InsertNewList(MSGDATALIST* list)
+{
+   if(FindList(list->listID) != NULL)
+   {
+      Serial.println("List already exists.");
+      return -1;
+   }
+   MSGDATALIST* oldFirstList = lists;
+   lists = list;
+   list->next = oldFirstList;
+   list->lastWritingTime = millis();
+   return 0;
+}
+
+MSGDATALIST* SimpleCAN::FindList(int listID)
+{
+   // Serial.print("Finding a list with listID: ");
+   // Serial.println(listID);
+   MSGDATALIST* currentList = lists;
+   while(currentList != NULL)
+   {
+      if(currentList->listID == listID)
+      {
+         // Serial.println("List found.");
+         return currentList;
+      }
+      currentList = currentList->next;
+   }
+   // Serial.println("List has not been found");
+   return NULL;
+}
+
+MSGDATALIST *SimpleCAN::FindListWithPreviousList(int listID, MSGDATALIST** previousList) // to do opschonen
+{
+   MSGDATALIST *_previousList = NULL;
+   MSGDATALIST *currentList = lists;
+   while (currentList != NULL)
+   {
+      if (currentList->listID == listID)
+      {
+         *previousList = _previousList;
+         return currentList;
+      }
+      _previousList = currentList;
+      currentList = currentList->next;
+   }
+   return NULL;
+}
+
+int SimpleCAN::InsertMsgData(msgDataList* list, MSGDATA* data)
+{
+   if(list == NULL || data == NULL)
+   {
+      return -1;
+   }
+   if(data->dataID < 0)
+   {
+      return -2;
+   }
+   if(list->dataList == NULL)
+   {
+      list->dataList = data;
+   }
+   else
+   {
+      MSGDATA* currentData = list->dataList;
+      while (currentData != NULL)
+      {
+         if(currentData->dataID == data->dataID)
+         {
+            return -3;
+         }
+         if(currentData->next != NULL)
+         {
+            if(currentData->dataID < data->dataID && currentData->next->dataID > data->dataID)
+            {
+               MSGDATA* tempNext = currentData->next;
+               currentData->next = data;
+               data->next = tempNext;
+               break;
+            } 
+         }
+         else
+         {
+            currentData->next = data;
+            break;
+         }
+         currentData = currentData->next;
+      }
+   }
+   list->totalData+=data->size;
+   list->listSize++;
+   list->lastWritingTime = millis();
+   return 0;
+}
+
+int SimpleCAN::AddMsgData(int listID, MSGDATA *newData, byte** allData, int *allDataSize)
+{
+   MSGDATALIST* list = FindList(listID);
+   if (list == NULL)
+   {
+      return -1;
+   }
+   if (newData == NULL || newData->dataID < 0 || newData->dataID > list->expectedListSize)
+   {
+      return -2;
+   }
+
+   int insertResult = InsertMsgData(list, newData) != 0;
+   if(insertResult == -2)
+   {
+      return -2;
+   }
+   if (insertResult == -3)
+   {
+      return -3;
+   }
+   
+   if(list->expectedListSize == list->listSize)
+   {
+      if(CompileListData(list, allData, allDataSize) == 0)
+      {
+         // Serial.print("CompiledListData: ");
+         // printByteArrayHex(*allData, *allDataSize);
+         // Serial.println();
+         // Serial.print("AllData address: ");
+         // int allDataPtr = (int)allData;
+         // Serial.println(allDataPtr);
+         // Serial.print("AllDataSize address: ");
+         // int allDataSizePtr = (int)allDataSize;
+         // Serial.println(allDataSizePtr);
+         // Serial.println("Removing list");
+
+         RemoveList(listID);
+         return 1;
+      } 
+   }
+   return 0;
+}
+
+int SimpleCAN::CompileListData(MSGDATALIST *list, byte** allData, int* allDataSize)
+{
+   // Serial.println("====== IN COMPILE ======");
+
+   if(list == NULL)
+   {
+      return -1;
+   }
+
+   byte *newArr = new byte[list->totalData];
+   *allData = newArr;
+
+   MSGDATA* currentData = list->dataList;
+   
+   size_t allDataIndex = 0;
+   while (currentData != NULL)
+   {
+      // Serial.print("Compiling data: ");
+      // printByteArrayHex(currentData->data, currentData->size);
+      // Serial.println();
+      for (size_t i = 0; i < (size_t)currentData->size; i++)
+      {
+         newArr[allDataIndex + i] = currentData->data[i];
+      }
+      allDataIndex += currentData->size;
+
+      // Serial.print("Compiled data so far: ");
+      // printByteArrayHex(newArr, allDataIndex);
+      // Serial.println();
+      
+      currentData = currentData->next;
+   }
+   *allDataSize = list->totalData;
+   if(list->expectedListSize != list->listSize)
+   {
+      return -2;
+   }
+   return 0;
+}
+int SimpleCAN::RemoveOldLists()
+{
+   // Serial.println("Removing old lists.");
+   int listsRemoved = 0;
+   MSGDATALIST *currentList = lists;
+   while (currentList != NULL)
+   {
+      if(currentList->lastWritingTime + extendedMessageTimeout > millis())
+      {
+         RemoveList(currentList->listID);
+         listsRemoved++;
+      }
+
+      currentList = currentList->next;
+   }
+   return listsRemoved;
+}
+
+int SimpleCAN::RemoveItemsInList(MSGDATALIST* list)
+{
+   if(list == NULL)
+   {
+      return -1;
+   }
+   MSGDATA *currentData = list->dataList;
+   while (currentData != NULL)
+   {
+      MSGDATA* tempNext = currentData->next;
+
+      delete[] currentData->data;
+      delete currentData;
+
+      currentData = tempNext;
+   }
+   return 0;
+}
+
+int SimpleCAN::RemoveList(int listID)
+{
+   MSGDATALIST* previousList = NULL;
+   MSGDATALIST *list = FindListWithPreviousList(listID, &previousList);
+   if (list == NULL)
+   {
+      return -1;
+   }
+
+   if(previousList == NULL) // list is first in list of lists
+   {
+      lists = list->next;
+   }
+   else
+   {
+      previousList->next = list->next;
+   }
+
+   RemoveItemsInList(list);
+   delete list;
+   return 0;
+}
--- Core/SimpleCAN/SimpleCAN.h
+++ Core/SimpleCAN/SimpleCAN.h
@@ -4,27 +4,51 @@
 #ifndef SIMPLECAN_H
 #define SIMPLECAN_H
 
+#include <stdlib.h>
 #include "MCP2515.h"
+
+typedef struct msgData
+{
+  int dataID;           // The number of this item in the list
+  byte *data;           // Data that came from a CANMSG
+  int size;             // Size of data
+  struct msgData *next; // Next item in list
+} MSGDATA;
+
+typedef struct msgDataList
+{
+  MSGDATA *dataList;
+  int listID;                     // Unique ID of the list
+  int listSize;                   // How many MSGDATA items are in the list
+  int expectedListSize;           // How many MSGDATA items have to be in the list for it to return a full message
+  int totalData;                  // How many bytes are saved in the list so far
+  unsigned long lastWritingTime;  // Last time a new item has been added to the list in milliseconds
+  struct msgDataList *next;       // Next item in metalist
+} MSGDATALIST;
 
 class SimpleCAN
 {
   public:
     SimpleCAN(void (*fun_ptr_arr[])(CANMSG), int startValue, int size);
+
+    // canTimeout is how many milliseconds to wait on the CANDIY shield for a new message
+    // UniqueSenderID is for sending extended messages, the ID has to be unique on the network, range: 0-255
     SimpleCAN(int canTimeout);
 
     void Setup();
 
     // Basic message sending and receiving
-    bool SendCANMSG(CANMSG msg);                                                       // Send message using the CANMSG struct.
-    bool SendByteMessage(byte buffer[], int size, unsigned long address);                 // Send message with a byte array, size of byte array and the address to send to.
-    bool SendCharMsg(char buffer[], int size, unsigned long address);                     // Send message with a char array, size of byte array and the address to send to.
-    bool SendUint32(uint32_t val, unsigned long address);                                 // Send a uint32 to address
-    bool SendInt(int val, unsigned long address);                                         // Send an int to address
-    bool SendBool(bool val, unsigned long address);                                       // Send a boolean to address
-    bool SendString(char buffer[], int size, uint8_t uniqueID, unsigned long adrsValue);  // Send message with a char array longer than 8 bytes, size of byte array, unique string ID who the message is from and the address to send to. Maximum size of string is 255*6 chars.
-    
-    bool GetReceivedMessage(CANMSG *msg);                                                 // Receive message through a pointer. False if no message is received.
-    bool GetReceivedMessageWithAddress(CANMSG *msg, unsigned long *address);              // Receive message through a pointer and get the address. False if no message is received.
+    bool SendCANMSG(CANMSG msg);                                                 // Send message using the CANMSG struct.
+    bool SendByteMessage(byte buffer[], int size, unsigned long adrsValue);      // Send message with a byte array, size of byte array and the address to send to.
+    bool SendCharMsg(char buffer[], int size, unsigned long address);            // Send message with a char array, size of byte array and the address to send to.
+    bool SendUint32(uint32_t val, unsigned long address);                        // Send a uint32 to address
+    bool SendInt(int val, unsigned long address);                                // Send an int to address
+    bool SendBool(bool val, unsigned long address);                              // Send a boolean to address
+    bool SendString(char buffer[], int size, unsigned long adrsValue, int senderID); // Send message with a char array longer than 8 bytes, size of char array and the address to send to. Maximum size of string is 255*6 chars.
+    bool SendLongByteMessage(byte buffer[], int size, unsigned long adrsValue, int senderID); // Send message with an array longer than 8 bytes, size of byte array and the address to send to. Maximum size of string is 255*6 chars.
+
+    bool GetReceivedMessage(CANMSG *msg);                                        // Receive message through a pointer. False if no message is received.
+    bool GetReceivedMessageWithAddress(CANMSG *msg, unsigned long *address);     // Receive message through a pointer and get the address. False if no message is received.
 
 
     // Conversions of types
@@ -42,11 +66,19 @@ class SimpleCAN
 
 
     void CANMSGToCharArray(CANMSG msg, char* str, int* size);                // Returns the char array and size of the array as pointers.
-    bool CANMSGToString(CANMSG msg, char* str, int* size);                   // Receives CANMSG structs, will return TRUE once it has a full message, returning the string as a char pointer with the string size. 
-    
+    bool CANMSGToString(CANMSG msg, char* str, int* size);                   // Receives CANMSG structs, will return TRUE once it has a full message, returning the string as a char pointer with the string size.
+    bool CANMSGToByteArray(CANMSG msg, byte** arr, int *size);                // Receives CANMSG structs, will return TRUE once it has a full message, returning the bytes in a pointer together with the array size.
 
     // Misc functions
     void printByteArray(byte *arrayPtr, int size);                           // Print a byte array, easy way to print CANMSG data by entering msg->data and msg->dataLength
+
+    // prints an array in hex on a single line, without ending the line
+    void printByteArrayHex(byte *data, int length);
+
+    // Prints list that holds received ExtendedMessage data
+    // returns 0 on success
+    // returns -1 if listID is invalid
+    int PrintList(int listID);
 
   private : 
     MCP2515 can;
@@ -56,6 +88,62 @@ class SimpleCAN
     void (**address_array)(CANMSG);
 
    bool BuildCanString(char buffer[], int size, unsigned long adrsValue);
+   bool SendPartOfLongByteMessage(byte buffer[], int size, byte uniqueMessageID, byte messageNumber, unsigned long adrsValue);
+
+   // Extended message handling
+
+   long extendedMessageTimeout; // Time before old messages are removed with RemoveOldMessages() function
+   MSGDATALIST* lists;
+   
+
+
+
+   // returns 0 on success
+   // returns -1 on failure of creating a new list
+   int CreateNewList(int listID, int expectedListSize);
+
+   // returns 0 on success
+   // returns -1 if this listID is already in use
+   int InsertNewList(MSGDATALIST *list);
+
+   // returns a list if found
+   // returns NULL if the list is not found
+   MSGDATALIST* FindList(int listID);
+
+   // returns a list if found
+   // returns NULL if the list is not found
+   MSGDATALIST *FindListWithPreviousList(int listID, MSGDATALIST **previousList);
+
+   // returns 0 on succes
+   // returns -1 if msgDataList or data is NULL
+   // returns -2 if data  is invalid
+   // returns -3 if data with the same dataID already exists 
+   int InsertMsgData(msgDataList* list, MSGDATA* data);
+
+   // returns 1 when full extendedMessage has been received, it will be returned through pointers and the list will be removed
+   // returns 0 when data has been added to list
+   // returns -1 if this list does not exist yet
+   // returns -2 if newData is NULL or invalid
+   // returns -3 if data with that ID already exists in the list
+   int AddMsgData(int listID, MSGDATA *newData, byte** allData, int* allDataSize);
+
+   // returns 0 on success
+   // returns -1 if list is NULL
+   // returns -2 if the list is not complete yet and extendedMessage has not been fully received, this will still compile the list and return it
+   int CompileListData(MSGDATALIST *list, byte **allData, int *allDataSize);
+
+   // This function checks the lastAccessTime of each list, if long ago enough it gets removed as last data is not expected to arrive anymore
+   // This function should be called periodically to clean up any lost messages
+   // returns the amount of lists removed
+   int RemoveOldLists(); 
+
+   // returns 0 if list successfully cleaned from MSGDATA objects
+   // returns -1 if listID is invalid
+   int RemoveItemsInList(MSGDATALIST *list);
+
+   // returns 0 if list successfully removed
+   // returns -1 if listID is invalid
+   int RemoveList(int listID); 
 };
 
 #endif
